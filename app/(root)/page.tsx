92e412ba44856375787118899e4dd193bc479c0c
shihab01118 <shahed.shihab.0@gmail.com>
2024-12-01 15:38:11 +0600

    sign-in and sign-up pages added using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
-const HomePage = () => {
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
 
   return (
     <div>
       <h1>Home</h1>
+
+      <UserButton />
     </div>
   );
 };
 
-export default HomePage;
+export default Home;
